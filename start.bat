aa6c1da5ec52e8d8a755e469fd14204d11e64420
MoTyaZ1 <matberendeev@gmail.com>
2025-12-21 03:52:37 +0300

    Адаптация цветов в консоли для Windows 10

--- start.bat
+++ start.bat
@@ -2,9 +2,12 @@
 chcp 65001 > nul
 title War Thunder Rich Presence
 
-echo  War Thunder Discord Rich Presence v1.0.0 (Release)
+:: Включаем поддержку ANSI цветов
+set ENABLE_VIRTUAL_TERMINAL_PROCESSING=1
+
+echo  War Thunder Discord Rich Presence v1.0.1 (Release)
 echo.
 
-python main.py
+py main.py
 
 pause
